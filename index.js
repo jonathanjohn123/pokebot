972a87b9ac81563408cb0e88412391dd48a16c01
jonathanjohn123 <49113382+jonathanjohn123@users.noreply.github.com>
2019-03-31 09:17:30 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const TelegramBot = require('node-telegram-bot-api');
 
 const port = process.env.PORT || 443,
     host = '0.0.0.0', // probably this change is not required
-    externalUrl = process.env.HOSTURL,
-    token = process.env.TOKEN || 'YOUR TELEGRAM TOKEN',
+    externalUrl = "https://johnnya10.herokuapp.com/",
+    token = process.env.TOKEN || '835310702:AAHHXTrDf1_LU-W6RP-DRCQun0d5t4WCwU8',
     bot = new TelegramBot(token, {
         webHook: {
             port: port,
@@ -11,3 +11,9 @@ const port = process.env.PORT || 443,
         }
     });
 bot.setWebHook(externalUrl + ':443/bot' + token);
+
+bot.on('message', (msg) => {
+    const chatId = msg.chat.id;
+    // send a message to the chat acknowledging receipt of their message
+    bot.sendMessage(chatId, 'hello maire jonathan here');
+  });
